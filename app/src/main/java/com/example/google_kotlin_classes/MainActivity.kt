6fbf9b10d1042b2f41694f6e10f48fb91d426932
RaphaelQSmith <49982690+RaphaelQSmith@users.noreply.github.com>
2021-03-16 15:38:21 -0300

    Birthday card example app

--- app/src/main/java/com/example/google_kotlin_classes/MainActivity.kt
+++ app/src/main/java/com/example/google_kotlin_classes/MainActivity.kt
@@ -10,11 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Hello World!", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
